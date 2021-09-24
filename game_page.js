3ab6699c6df8c61721e04d3a3fb8501118fe11a5
Advath Coder <72031535+Advaith6@users.noreply.github.com>
2021-09-24 16:35:46 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -4,6 +4,8 @@ player2_name = localStorage.getItem("player2_name");
 player1_score = 0;
 player2_score = 0;
 
+actual_answer = 0;
+
 document.getElementById("player1_name").innerHTML = player1_name+" : ";
 document.getElementById("player2_name").innerHTML = player2_name+" : ";
 
@@ -21,7 +23,41 @@ function send(){
     question_number = "<h4> Q. "+number1+" X "+number2+"</h4>";
     input_box = "<br>Answer : <input type = 'text' id = 'input_check_box'>";
     check_button = "<br><br><button class = 'btn btn-info' onclick  = 'check()'>Check</button>";
-    row = question_Word+input_box+check_button;
+    row = question_number+input_box+check_button;
     document.getElementById("output").innerHTML = row;
     document.getElementById("word").value = "";
+}
+
+question_turn = "player1";
+answer_turn = "player2";
+
+function check(){
+    get_answer = document.getElementById("input_check_box").value;
+    if(get_answer == actual_answer){
+        if(answer_turn == "player1"){
+            player1_score = player1_score +1;
+            document.getElementById("player1_score").innerHTML = player1_score;
+        }
+        else{
+            player2_score = player2_score +1;
+            document.getElementById("player2_score").innerHTML = player2_score;   
+        }
+    }
+    if(question_turn == "player1"){
+        question_turn = "player2";
+        document.getElementById("player_question").innerHTML = "Question Turn - "+player2_name;
+    }
+    else{
+        question_turn = "player1";
+        document.getElementById("player_question").innerHTML = "Question Turn - "+player1_name;
+    }
+    if(answer_turn == "player1"){
+        answer_turn = "player2";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - "+player2_name;
+    }
+    else{
+        answer_turn = "player1";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - "+player1_name;
+    }
+    document.getElementById("output").innerHTML = "";
 }
